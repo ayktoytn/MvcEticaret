dd3dfeb5812c39b8c5e8df4535e2d1b6c456ce2a
ayktoytn <oytun.aykut@gmail.com>
2023-08-11 23:07:39 +0300

    All the structure has been created regarding the Bootstrap structure and customizations have been made upon the request.

---
+++ MvcEticaret/Controllers/ProductItem.cs
@@ -0,0 +1,32 @@
+﻿namespace MvcEticaret.Models
+{
+    using Microsoft.AspNetCore.Mvc;
+
+    public class ProductItemViewComponent : ViewComponent
+    {
+        public IViewComponentResult Invoke(string title, int startCount, double oldPrice, double price, string imageUrl, bool isSale)
+        {
+            var model = new ProductItemViewModel
+            {
+                Title = title,
+                StartCount = startCount,
+                OldPrice = oldPrice,
+                Price = price,
+                ImageUrl = imageUrl,
+                IsSale = isSale
+            };
+
+            return View(model);
+        }
+    }
+
+    public class ProductItemViewModel
+    {
+        public string Title { get; set; }
+        public int StartCount { get; set; }
+        public double OldPrice { get; set; }
+        public double Price { get; set; }
+        public string ImageUrl { get; set; }
+        public bool IsSale { get; set; }
+    }
+}
